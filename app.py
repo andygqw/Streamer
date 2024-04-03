1e241c538ff2984463855fbd531f1942f8dd2873
andygqw <andygqwang@gmail.com>
2024-04-03 00:16:24 -0700

    video streaming partial

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, Response, stream_with_context
+from flask import Flask, render_template, Response, stream_with_context, request
+import re
 import os
 
 app = Flask(__name__)
@@ -14,7 +15,42 @@ def video_feed():
 @app.route('/video/<filePath>')
 def video(filePath):
     filePath = 'static/video/虽然是精神病但没关系_第1集.mp4'
-    return Response(stream_with_context(generate_video(filePath)), mimetype='video/mp4')
+
+    range_header = request.headers.get('Range', None)
+    if not range_header:  # Browser doesn't support range requests
+        print('Browser doesn\'t support range requests')
+        return Response(stream_with_context(generate_video(filePath)), mimetype='video/mp4')
+
+    size = os.path.getsize(filePath)    
+    byte1, byte2 = 0, None
+
+    # Extract the range values
+    m = re.search('(\d+)-(\d*)', range_header)
+    g = m.groups()
+
+    if g[0]:
+        byte1 = int(g[0])
+    if g[1]:
+        byte2 = int(g[1])
+
+    length = size - byte1
+    if byte2 is not None:
+        length = byte2 + 1 - byte1
+
+    # Create the initial response
+    data = None
+    with open(filePath, 'rb') as f:
+        f.seek(byte1)
+        data = f.read(length)
+
+    rv = Response(data, 
+                  206,  # 206 Partial Content
+                  mimetype='video/mp4',
+                  direct_passthrough=True)
+    rv.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(byte1, byte1 + length - 1, size))
+    rv.headers.add('Accept-Ranges', 'bytes')
+
+    return rv
 
 def generate_video(video_path):
     with open(video_path, "rb") as video_file:
