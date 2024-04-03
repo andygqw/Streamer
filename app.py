8c28423075034cfc0081a8ec39d6daf5e63dc6c1
andygqw <andygqwang@gmail.com>
2024-04-02 23:02:23 -0700

    added streaming method and html

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, stream_with_context
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/video/<filename>')
+def video(filename):
+    def generate():
+        with open(f'static/videos/{filename}', 'rb') as video:
+            data = video.read(1024)
+            while data:
+                yield data
+                data = video.read(1024)
+
+    return Response(stream_with_context(generate()), mimetype='video/mp4')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
