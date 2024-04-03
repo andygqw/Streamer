035b4db6aec6195c8ea97dbd67504853faca3d32
andygqw <andygqwang@gmail.com>
2024-04-02 23:41:37 -0700

    testing

--- app.py
+++ app.py
@@ -7,16 +7,24 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/video/<filename>')
-def video(filename):
+@app.route('/video')
+def video_feed():
+    return render_template('video.html')
+
+#@app.route('/video/<filename>')
+@app.route('/video1')
+#def video(filename):
+def video1():
     def generate():
         try:
+            filename = "static/video/虽然是精神病但没关系_第1集.mp4"
             with open(f'{filename}', 'rb') as video:
                 data = video.read(1024)
                 while data:
                     yield data
                     data = video.read(1024)
-        except FileNotFoundError:
+        except Exception as e:
+            print(str(e))
             return 'File not found'
 
     return Response(stream_with_context(generate()), mimetype='video/mp4')
